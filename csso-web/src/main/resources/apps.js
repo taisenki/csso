4081f8ba9b93a1b3c53ffcf95ad29cf104eda604
taisenki <taisenki@163.com>
2017-11-25 09:40:07 +0800

    default is localhost

--- csso-web/src/main/resources/apps.js
+++ csso-web/src/main/resources/apps.js
@@ -1,4 +1,4 @@
  [
 	 {appId:"1", appName:"csso", cssoServer:true},
-	 {appId:"1001", appName:"csso-app", host:"http://10.1.84.166:8080/csso-app", logoutUrl:"http://10.1.84.166:8080/csso-app/logout.do"}
+	 {appId:"1001", appName:"csso-app", host:"http://localhost:8080/csso-app", logoutUrl:"http://localhost:8080/csso-app/logout.do"}
 ]
