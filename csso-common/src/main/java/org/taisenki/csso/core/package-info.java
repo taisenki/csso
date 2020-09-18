7fabb2d2e5d6e28e3c4200e58b19d64fd20ad5d2
Taisenki <taisenki@163.com>
2020-09-18 09:02:26 +0800

    Update package-info.java

--- csso-common/src/main/java/org/taisenki/csso/core/package-info.java
+++ csso-common/src/main/java/org/taisenki/csso/core/package-info.java
@@ -3,7 +3,7 @@
  * File Name:package-info.java 
  * Package Name:org.taisenki.csso.core 
  * Date:2017年11月20日下午1:58:11 
- * Copyright (c) 2017, taisenki@dareway.com。cn All Rights Reserved.  
+ * Copyright (c) 2017, taisenki@163.com All Rights Reserved.  
  */
 /** 
  * 存放主接口和公用处理方法 <br/> 
